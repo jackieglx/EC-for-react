988ad696d8c55f1d3a8a06763cf764a2ade4bc96
glx513 <guo.lingx@northeastern.edu>
2024-10-25 22:41:01 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import {useState} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <div className="container">
+                <header>
+                    <h1>Lynn's Website</h1>
+                </header>
+
+                <aside className="left-sidebar"></aside>
+
+                <main>
+                    <div className="center-box"></div>
+                </main>
+
+                <aside className="right-sidebar"></aside>
+
+                <footer></footer>
+            </div>
+        </>
+    )
+}
+
+export default App
