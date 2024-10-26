b2c203eecf213347f8ab3d9c18c8321ae9757c6e
glx513 <guo.lingx@northeastern.edu>
2024-10-25 23:00:27 -0700

    ma5

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,51 @@
 import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+    const rows = 3;
+    const numBoxes = rows * rows;
+
+    //   0       1      2   ...  8
+    // [false, false, false....false]
+    const [boxesArray, setBoxesArray] = useState(Array(numBoxes).fill(false));
+
+
+    const toggleColor = (index) => {
+        setBoxesArray((prevSelected) => {
+            const newBoxesArray = [...prevSelected];
+            newBoxesArray[index] = !newBoxesArray[index];
+            return newBoxesArray;
+        });
+    };
+
+    const handleClick = (index) => {
+        const col = index % rows;
+        const fourDirections = {
+            up: index - rows,
+            down: index + rows,
+            left: index - 1,
+            right: index + 1,
+        };
+
+        toggleColor(index);
+
+        if (fourDirections.up >= 0) toggleColor(fourDirections.up);
+        if (fourDirections.down < numBoxes) toggleColor(fourDirections.down);
+        if (col > 0) toggleColor(fourDirections.left);
+        if (col < rows - 1) toggleColor(fourDirections.right);
+    };
 
     return (
         <>
             <div className="container">
-                <header>
-                    <h1>Lynn's Website</h1>
-                </header>
+                {Array.from({ length: numBoxes }).map((_, index) => (
+                    <div
+                        key={index}
+                        className={`box ${boxesArray[index] ? 'selected' : ''}`}
+                        onClick={() => handleClick(index)}
+                    ></div>
+                ))}
 
-                <aside className="left-sidebar"></aside>
-
-                <main>
-                    <div className="center-box"></div>
-                </main>
-
-                <aside className="right-sidebar"></aside>
-
-                <footer></footer>
             </div>
         </>
     )
